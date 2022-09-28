c22927a9469c7e69697009d6525545d401156379
zhangmingwei <zhangmingwei@chunsutech.com>
2022-09-28 09:52:31 +0800

    fixed:(test_product.py)

---
+++ cases/test_product.py
@@ -0,0 +1,64 @@
+import os
+import sys
+sys.path.append(os.getcwd())
+
+import pytest
+from page_admin import login_page, product_page
+from faker import Faker
+
+faker = Faker()
+params = [("admin","qwer@1234","登录成功！")]
+
+@pytest.fixture(scope="module")
+def login(request):
+     
+    pw = login_page.LoginPage()
+    tip = pw.login(request.param[0], request.param[1])
+    assert tip == request.param[2]
+    return pw.page   
+
+@pytest.mark.parametrize("login", params, indirect=True )
+class TestProduct:
+
+    productname = str(faker.name()*3)
+    picname = "./pic.jpeg"
+    price = "300"
+    stock = "99"
+    transPrice = "10"
+
+    def test_createproduct(self, login):
+        expect = [(
+            self.productname,
+            self.picname,
+            self.price,
+            self.stock,
+            self.transPrice
+        )
+        ]
+        for data in expect:
+            pw = product_page.ProductPage(login)
+            pw.CreateBasicProduct(
+                data[0],
+                data[1],
+                data[2],
+                data[3],
+                data[4]  
+            )
+        pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    pytest.main(["cases/test_product.py","-vs"])
