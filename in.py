6fe5290b56fc8d35b1a8df1f4a69437f9a4e57eb
zhangmingwei <zhangmingwei@chunsutech.com>
2022-09-29 17:53:09 +0800

    fixed:(in.py)

--- in.py
+++ in.py
@@ -52,9 +52,12 @@ def createCategory(categoryname):
             data=payload_category)
     return response_category
 
+# def createProduct(productname,categoryname,skucode,categories_id):
+
+   
 
 
-list_Xlinex = getXlinex(limitnumber = "1")
+list_Xlinex = getXlinex(limitnumber = "4000")
 for i in list_Xlinex:
    productname = i['itemName']
    categoryname = i['brand']
@@ -66,109 +69,99 @@ for i in list_Xlinex:
 
 #  #  Category List 
    response_categories = getCategories()
-   print(f'response_categories:{response_categories}')
+   # print(f'response_categories:{response_categories}')
 #  #  get Category_id & Category_name
    list_categories = {}
-   for j in response_categories:
+   for j in response_categories[0]['children']:
       # print(j['name'])
       # print(j['id'])
       list_categories[j['name']]=j['id']
-   print(f'list_categories:{list_categories}')
+   # print(f'list_categories:{list_categories}')
 #  #  if else Category had been exited
    for k in list_categories:
-      if k == categoryname:
+      if str(k) == str(categoryname):
          categories_id = list_categories[k]
          print(f"categories_id:{categories_id}")
+         break
       else:
          create_category = createCategory(categoryname)
          print(f'create_category:{create_category,create_category.json()}')
-         response_categories = getCategories()
-         print(f'response_categories:{response_categories}')
+         categories_id = str(create_category.json()['id'])
+         # response_categories = getCategories()
+         print(f'categories_id:{categories_id}')
+         # print("123")
+      
+
+   url = "https://gateway-tg-test.chunsutech.com/admin/product"
+   payload = json.dumps({
+      "userId": "1",
+      "name": productname,
+      "preSaleEnd": {
+   "seconds": 20,
+   "nanos": 10
+},
+      "preSaleRate": 57,
+      "overbook": False,
+      "balanceStart": {
+   "seconds": 20,
+   "nanos": 10
+},
+         "pic": [],
+      "deliveryType": 1,
+      "upSellsIds": [
+         59
+      ],
+      "deliveryDay": 20,
+      "balanceEnd": {
+   "seconds": 20,
+   "nanos": 10
+},
+      "location": "voluptate",
+      "preSaleStart": {
+   "seconds": 20,
+   "nanos": 10
+},
+      "preSaleAmount": 65,
+      "sizeX": 64,
+      "transPrice": 80,
+      "describe": "non",
+      "isPublish": False,
+      "locationId": 85,
+      "templateId": 0,
+      "relatedItemIds": [
+         79
+      ],
+      "specs": [
+      ],
+      "spu": skucode,
+      "crossSellIds": [
+         63
+      ],
+      "sku": [
+         {
+            "adminSku": skucode,
+            "variations": [],
+            "stock": 79,
+            "barCode": "57",
+            "weight": 73,
+            "pic": [],
+            "price": 45
+         }
+      ],
+      "sizeY": 85,
+      "sizeZ": 1,
+      "preSale": False,
+      "categoryIds": [
+         categories_id
          
+      ]
+   })
+   headers = {
+      'X-Tester': '1',
+      'Content-Type': 'application/json'
+   }
 
-         
+   response_createproduct = requests.request("POST", url, headers=headers, data=payload)
 
+   print(f'response_createproduct:{response_createproduct,response_createproduct.json()}')
 
-   
-
-# #    url = "https://gateway-tg-test.chunsutech.com/admin/product"
-# #    payload = json.dumps({
-# #       "userId": "1",
-# #       "name": i['itemName'],
-# #       "preSaleEnd": {
-# #     "seconds": 20,
-# #     "nanos": 10
-# #   },
-# #       "preSaleRate": 57,
-# #       "overbook": False,
-# #       "balanceStart": {
-# #     "seconds": 20,
-# #     "nanos": 10
-# #   },
-# #       "pic": [
-# #          {
-# #             "path": "https://www.baidu.com/img/PCtm_d9c8750bed0b3c7d089fa7d55720d6cf.png"
-# #          }
-# #       ],
-# #       "deliveryType": 1,
-# #       "upSellsIds": [
-# #          59
-# #       ],
-# #       "deliveryDay": 20,
-# #       "balanceEnd": {
-# #     "seconds": 20,
-# #     "nanos": 10
-# #   },
-# #       "location": "voluptate",
-# #       "preSaleStart": {
-# #     "seconds": 20,
-# #     "nanos": 10
-# #   },
-# #       "preSaleAmount": 65,
-# #       "sizeX": 64,
-# #       "transPrice": 80,
-# #       "describe": "non",
-# #       "isPublish": False,
-# #       "locationId": 85,
-# #       "templateId": 73,
-# #       "relatedItemIds": [
-# #          79
-# #       ],
-# #       "specs": [
-# #       ],
-# #       "spu": "commodo ut amet",
-# #       "crossSellIds": [
-# #          63
-# #       ],
-# #       "sku": [
-# #          {
-# #             "adminSku": i['itemCode'],
-# #             "variations": [],
-# #             "stock": 79,
-# #             "barCode": "57",
-# #             "weight": 73,
-# #             "pic": [{
-# #             "path": "https://www.baidu.com/img/PCtm_d9c8750bed0b3c7d089fa7d55720d6cf.png"
-# #          }],
-# #             "price": 45
-# #          }
-# #       ],
-# #       "sizeY": 85,
-# #       "sizeZ": 1,
-# #       "preSale": False,
-# #       "categoryIds": [
-# #          "2",
-# #          "82",
-# #          "83",
-# #          "49"
-# #       ]
-# #    })
-# #    headers = {
-# #       'X-Tester': '1',
-# #       'Content-Type': 'application/json'
-# #    }
-
-# #    response = requests.request("POST", url, headers=headers, data=payload)
-
-# #    print(response.json())
-# #    print(response.text)
