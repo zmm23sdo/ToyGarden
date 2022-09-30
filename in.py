68cd8ae3fd70234adb5367b8ce12dbbe35b21c96
zhangmingwei <zhangmingwei@chunsutech.com>
2022-09-30 08:24:28 +0800

    fixed:(in.py)

--- in.py
+++ in.py
@@ -56,112 +56,125 @@ def createCategory(categoryname):
 
    
 
+def list_create(offsetnumber,limitnumber):
+   list_Xlinex = getXlinex(offsetnumber = offsetnumber, limitnumber = limitnumber)
+   for i in list_Xlinex:
+      productname = i['itemName']
+      categoryname = i['brand']
+      skucode = i['itemCode']
 
-list_Xlinex = getXlinex(offsetnumber = "2000", limitnumber = "1000")
-for i in list_Xlinex:
-   productname = i['itemName']
-   categoryname = i['brand']
-   skucode = i['itemCode']
+      print(f'productname:{productname}')
+      print(f'categoryname:{categoryname}')
+      print(f'skucode:{skucode}')
 
-   print(f'productname:{productname}')
-   print(f'categoryname:{categoryname}')
-   print(f'skucode:{skucode}')
-
-#  #  Category List 
-   response_categories = getCategories()
-   # print(f'response_categories:{response_categories}')
-#  #  get Category_id & Category_name
-   list_categories = {}
-   for j in response_categories[0]['children']:
-      # print(j['name'])
-      # print(j['id'])
-      list_categories[j['name']]=j['id']
-   # print(f'list_categories:{list_categories}')
-#  #  if else Category had been exited
-   for k in list_categories:
-      if str(k) == str(categoryname):
-         categories_id = list_categories[k]
-         print(f"categories_id:{categories_id}")
-         break
-      else:
-         create_category = createCategory(categoryname)
-         print(f'create_category:{create_category,create_category.json()}')
-         categories_id = str(create_category.json()['id'])
-         # response_categories = getCategories()
-         print(f'categories_id:{categories_id}')
-         # print("123")
-      
-
-   url = "https://gateway-tg-test.chunsutech.com/admin/product"
-   payload = json.dumps({
-      "userId": "1",
-      "name": productname,
-      "preSaleEnd": {
-   "seconds": 20,
-   "nanos": 10
-},
-      "preSaleRate": 57,
-      "overbook": False,
-      "balanceStart": {
-   "seconds": 20,
-   "nanos": 10
-},
-         "pic": [],
-      "deliveryType": 1,
-      "upSellsIds": [
-         59
-      ],
-      "deliveryDay": 20,
-      "balanceEnd": {
-   "seconds": 20,
-   "nanos": 10
-},
-      "location": "voluptate",
-      "preSaleStart": {
-   "seconds": 20,
-   "nanos": 10
-},
-      "preSaleAmount": 65,
-      "sizeX": 64,
-      "transPrice": 80,
-      "describe": "non",
-      "isPublish": False,
-      "locationId": 85,
-      "templateId": 0,
-      "relatedItemIds": [
-         79
-      ],
-      "specs": [
-      ],
-      "spu": skucode,
-      "crossSellIds": [
-         63
-      ],
-      "sku": [
-         {
-            "adminSku": skucode,
-            "variations": [],
-            "stock": 79,
-            "barCode": "57",
-            "weight": 73,
-            "pic": [],
-            "price": 45
-         }
-      ],
-      "sizeY": 85,
-      "sizeZ": 1,
-      "preSale": False,
-      "categoryIds": [
-         categories_id
+   #  #  Category List 
+      response_categories = getCategories()
+      # print(f'response_categories:{response_categories}')
+   #  #  get Category_id & Category_name
+      list_categories = {}
+      for j in response_categories[0]['children']:
+         # print(j['name'])
+         # print(j['id'])
+         list_categories[j['name']]=j['id']
+      # print(f'list_categories:{list_categories}')
+   #  #  if else Category had been exited
+      for k in list_categories:
+         if str(k) == str(categoryname):
+            categories_id = list_categories[k]
+            print(f"categories_id:{categories_id}")
+            break
+         else:
+            create_category = createCategory(categoryname)
+            print(f'create_category:{create_category,create_category.json()}')
+            categories_id = str(create_category.json()['id'])
+            # response_categories = getCategories()
+            print(f'categories_id:{categories_id}')
+            break
+            # print("123")
          
-      ]
-   })
-   headers = {
-      'X-Tester': '1',
-      'Content-Type': 'application/json'
-   }
 
-   response_createproduct = requests.request("POST", url, headers=headers, data=payload)
+      url = "https://gateway-tg-test.chunsutech.com/admin/product"
+      payload = json.dumps({
+         "userId": "1",
+         "name": productname,
+         "preSaleEnd": {
+      "seconds": 20,
+      "nanos": 10
+   },
+         "preSaleRate": 57,
+         "overbook": False,
+         "balanceStart": {
+      "seconds": 20,
+      "nanos": 10
+   },
+            "pic": [],
+         "deliveryType": 1,
+         "upSellsIds": [
+            59
+         ],
+         "deliveryDay": 20,
+         "balanceEnd": {
+      "seconds": 20,
+      "nanos": 10
+   },
+         "location": "voluptate",
+         "preSaleStart": {
+      "seconds": 20,
+      "nanos": 10
+   },
+         "preSaleAmount": 65,
+         "sizeX": 64,
+         "transPrice": 80,
+         "describe": "non",
+         "isPublish": False,
+         "locationId": 85,
+         "templateId": 0,
+         "relatedItemIds": [
+            79
+         ],
+         "specs": [
+         ],
+         "spu": skucode,
+         "crossSellIds": [
+            63
+         ],
+         "sku": [
+            {
+               "adminSku": skucode,
+               "variations": [],
+               "stock": 79,
+               "barCode": "57",
+               "weight": 73,
+               "pic": [],
+               "price": 45
+            }
+         ],
+         "sizeY": 85,
+         "sizeZ": 1,
+         "preSale": False,
+         "categoryIds": [
+            categories_id
+            
+         ]
+      })
+      headers = {
+         'X-Tester': '1',
+         'Content-Type': 'application/json'
+      }
 
-   print(f'response_createproduct:{response_createproduct,response_createproduct.json()},success!','\n','='*100)
+      response_createproduct = requests.request("POST", url, headers=headers, data=payload)
+
+      print(f'response_createproduct:{response_createproduct,response_createproduct.json()},success!','\n','='*100)
+
+list_create(offsetnumber = "0",limitnumber = "1000")
+list_create(offsetnumber = "1000",limitnumber = "1000")
+list_create(offsetnumber = "2000",limitnumber = "1000")
+list_create(offsetnumber = "3000",limitnumber = "1000")
+list_create(offsetnumber = "4000",limitnumber = "1000")
+list_create(offsetnumber = "5000",limitnumber = "1000")
+list_create(offsetnumber = "6000",limitnumber = "1000")
+
+
+
+
 
