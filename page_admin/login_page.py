338843c7bdbf8ad13b6a9d4ab0cd426adc5c6459
zhangmingwei <zhangmingwei@chunsutech.com>
2022-10-17 17:56:38 +0800

    refactor(login): Put the dom element back into the package

--- page_admin/login_page.py
+++ page_admin/login_page.py
@@ -2,13 +2,17 @@ from base.base_page import BasePage
 
 class LoginPage(BasePage):
     admin_url = "https://admin-tg-test.chunsutech.com/"
-    locater_username = "#user_name"
-    locater_password = "#password"
-    button_in = ".ant-btn.ant-btn-primary.ant-btn-lg"
-    bubble = ".ant-message-notice-content"
     dashboard_url = "https://admin-tg-test.chunsutech.com/dashboard"
-    header_icon = ".ant-dropdown-trigger.action___LP4_P.account___6HXOq"
-    logout_button = ".ant-dropdown-menu-item"
+
+    # locater_username = "#user_name"
+    # locater_password = "#password"
+    # button_in = ".ant-btn.ant-btn-primary.ant-btn-lg"
+    # bubble = ".ant-message-notice-content"
+    # header_icon = ".ant-dropdown-trigger.action___LP4_P.account___6HXOq"
+    # logout_button = ".ant-dropdown-menu-item"
+
+    # error_user_text = "text=用户名是必填项！"
+    # error_pwd_text = "text=密码是必填项！"
 
     def __init__(self) -> None:
         super().__init__()
@@ -16,63 +20,62 @@ class LoginPage(BasePage):
 
     def login(self, username, password):
         self.visit(self.admin_url)
-        self.fill(self.locater_username, username)
-        self.fill(self.locater_password, password)
+        self.fill("#user_name", username)
+        self.fill("#password", password)
           # Click button:has-text("登 录")
-        self.click(self.button_in)
+        self.click(".ant-btn.ant-btn-primary.ant-btn-lg")
         # assert page.url == "https://admin-tg-dev.chunsutech.com/dashboard"
         # Click .ant-message-notice-content
-        self.click(self.bubble)
-        tip = self.page.text_content(self.bubble)
+        self.click( ".ant-message-notice-content")
+        tip = self.page.text_content( ".ant-message-notice-content")
         return tip
     
-    error_user_text = "text=用户名是必填项！"
-    error_pwd_text = "text=密码是必填项！"
+    
 
     def login_empty(self, username, password):
         self.visit(self.admin_url)
-        self.fill(self.locater_username, username)
-        self.fill(self.locater_password, password)
+        self.fill("#user_name", username)
+        self.fill("#password", password)
           # Click button:has-text("登 录")
-        self.click(self.button_in)
+        self.click(".ant-btn.ant-btn-primary.ant-btn-lg")
         # Click text=用户名是必填项！
-        self.click(self.error_user_text)
+        self.click("text=用户名是必填项！")
         # Click text=密码是必填项！
-        self.click(self.error_pwd_text)
-        tip_user = self.page.text_content(self.error_user_text)
-        tip_pwd = self.page.text_content(self.error_pwd_text)
+        self.click("text=密码是必填项！")
+        tip_user = self.page.text_content("text=用户名是必填项！")
+        tip_pwd = self.page.text_content("text=密码是必填项！")
         return tip_user, tip_pwd
         
     def login_empty_username(self, username, password):
         self.visit(self.admin_url)
-        self.fill(self.locater_username, username)
-        self.fill(self.locater_password, password)
+        self.fill("#user_name", username)
+        self.fill("#password", password)
           # Click button:has-text("登 录")
-        self.click(self.button_in)
+        self.click(".ant-btn.ant-btn-primary.ant-btn-lg")
         # Click text=用户名是必填项！
-        self.click(self.error_user_text)
+        self.click("text=用户名是必填项！")
         # # Click text=密码是必填项！
-        # self.click(self.error_pwd_text)
-        tip_user = self.page.text_content(self.error_user_text)
-        # tip_pwd = self.page.text_content(self.error_pwd_text)
+        # self.click("text=密码是必填项！")
+        tip_user = self.page.text_content("text=用户名是必填项！")
+        # tip_pwd = self.page.text_content("text=密码是必填项！")
         return tip_user
 
     def login_empty_password(self, username, password):
         self.visit(self.admin_url)
-        self.fill(self.locater_username, username)
-        self.fill(self.locater_password, password)
+        self.fill("#user_name", username)
+        self.fill("#password", password)
           # Click button:has-text("登 录")
-        self.click(self.button_in)
+        self.click(".ant-btn.ant-btn-primary.ant-btn-lg")
         # # Click text=用户名是必填项！
-        # self.click(self.error_user_text)
+        # self.click("text=用户名是必填项！")
         # Click text=密码是必填项！
-        self.click(self.error_pwd_text)
-        # tip_user = self.page.text_content(self.error_user_text)
-        tip_pwd = self.page.text_content(self.error_pwd_text)
+        self.click("text=密码是必填项！")
+        # tip_user = self.page.text_content("text=用户名是必填项！")
+        tip_pwd = self.page.text_content("text=密码是必填项！")
         return tip_pwd
 
     def loginout(self):
-        self.visit(self.dashboard_url)
-        self.click(self.header_icon)
-        self.click(self.logout_button)
+        self.visit("https://admin-tg-test.chunsutech.com/dashboard")
+        self.click(".ant-dropdown-trigger.action___LP4_P.account___6HXOq")
+        self.click(".ant-dropdown-menu-item")
         # assert page.url == "https://admin-tg-test.chunsutech.com/user/login?redirect=%2Fdashboard"
