694004d15de785a563f1cab5f38e47c0a1adc0a0
zhangmingwei <zhangmingwei@chunsutech.com>
2022-10-18 09:16:10 +0800

    refactor((admin)): put the dom element back into

---
+++ page_admin/users_page/roles_page.py
@@ -0,0 +1,36 @@
+from base.base_page import BasePage
+
+class RolePage(BasePage):
+
+    def __init__(self, page) -> None:
+        super().__init__()
+        self.page = page
+    
+
+    role_url = "https://admin-tg-test.chunsutech.com/users/roles"
+    role_create_url = "https://admin-tg-test.chunsutech.com/users/roles/create"
+
+    def CreateRole(self, rolename, description):
+        # Go to https://admin-tg-test.chunsutech.com/users/roles
+        self.visit(self.role_url)
+        # Click button:has-text("Create Role")
+        self.click(".ant-btn.ant-btn-primary")
+        self.wait(3000)
+        # Fill [placeholder="Cannot exceed 30 characters"]
+        self.fill("#name", rolename)
+        # Fill textarea:has-text("role")
+        self.fill("#description", description)
+        # Check input[type="checkbox"]
+        self.locator(".ant-checkbox-input").check()
+        # Click button:has-text("confirm")
+        self.click(".confirm___G2-JO")
+        # Click .ant-message-notice-content
+        self.click(".ant-message-notice-content")
+
+    def SearchRole(self, search_data):
+        # Fill [placeholder="commodity\.list\.search\.placeholder"]
+        self.fill(".ant-input-wrapper.ant-input-group", search_data)
+        # Click button >> nth=1
+        self.click(".ant-input-group-addon")
+        # Click [aria-label="reload"] svg
+        self.click(".anticon.anticon-reload")
--- page_admin/users_page/users_page.py
+++ page_admin/users_page/users_page.py
@@ -6,6 +6,8 @@ class UserPage(BasePage):
         super().__init__()
         self.page = page
 
+    user_url = "https://admin-tg-test.chunsutech.com/users/users"
+
     def CreateUser(self, username, fullname, email, password):
         # Go to https://admin-tg-test.chunsutech.com/users/users
         self.visit(self.user_url)
@@ -28,3 +30,13 @@ class UserPage(BasePage):
         self.fill("#password", password)
         # Click button:has-text("确 定")
         self.click(".ant-space-item>.ant-btn.ant-btn-primary" )
+
+    def SearchUser(self, search_data):
+        # Go to https://admin-tg-test.chunsutech.com/users/users
+        self.visit(self.user_url)
+        # Fill [placeholder="commodity\.list\.search\.placeholder"]
+        self.fill(".ant-input-wrapper.ant-input-group", search_data)
+        # Click button >> nth=1
+        self.click(".ant-btn.ant-btn-default.ant-btn-icon-only.ant-input-search-button")
+        # Click [aria-label="reload"] svg
+        self.click(".anticon.anticon-reload")
